a1a307edd42c355af629bb505684d7b48a37c406
bcrabbe <bencrabbe@hotmail.co.uk>
2015-06-04 21:12:11 +0100

    fixed bug

--- app/routes/index.js
+++ app/routes/index.js
@@ -19,20 +19,12 @@ var express = require('express'),
 /* GET home page. */
 router.get('/', function(req, res) {
     var accountController = new AccountController(User, req.session);
-    console.log("here1");
     var signedIn = accountController.session.userProfileModel !== undefined ? 1 : 0;
-    console.log("here2");
-
-    //Author search
     if(req.query.author !== undefined) {
         var author = req.query.author;
         Post.find().where('author').equals(author).sort({ created: -1 }).limit(10).exec(function(err, authorsPosts) {
             if (err) return res.send("error");
-            console.log("\n\nAuthorsPosts:" +authorsPosts);
-            console.log("\n\authorsPosts.length: " +authorsPosts.length);
-            console.log("authors post.constructor = " +authorsPosts.constructor);
             if(authorsPosts.length==0) {
-                console.log("length=0");
                 res.render('pages/index', {
                     viewDataSignStatus: viewDataSignedIn[signedIn],
                     previews: authorsPosts,
@@ -45,38 +37,32 @@ router.get('/', function(req, res) {
                 });
             }
         });
-    }
-    //Tag search
-    if(req.query.filter !== undefined) {
+    } else if(req.query.filter !== undefined) {
         var tagList = req.query.filter.constructor == Array ? req.query.filter : req.query.filter.split(",");
         Post.find( { tags : { $elemMatch: { $in : tagList } } } ).limit(10).exec(function(err, taggedPosts) {
             if (err) return res.send("error");
-            console.log("\n\taggedPosts.length: " +taggedPosts.length);
             if(taggedPosts.length==0) {
-                console.log("length=0");
-                
                 res.render('pages/index', {
                     viewDataSignStatus: viewDataSignedIn[signedIn],
                     previews: taggedPosts,
                     error: "Sorry there are no posts with that tag."
                 });
             } else {
-                console.log("\n\ntaggedPosts:\n\n" +taggedPosts);
                 res.render('pages/index', {
                     viewDataSignStatus: viewDataSignedIn[signedIn],
                     previews: taggedPosts
                 });
             }
         });
-    }
-    //or just latest
-    Post.find().sort({ created: 1 }).limit(10).exec(function(err, latestPosts) {
-        if (err) return res.send(err);
-        res.render('pages/index', {
-            viewDataSignStatus: viewDataSignedIn[signedIn],
-            previews: latestPosts
+    } else {
+        Post.find().sort({ created: 1 }).limit(10).exec(function(err, latestPosts) {
+            if (err) return res.send(err);
+            res.render('pages/index', {
+                viewDataSignStatus: viewDataSignedIn[signedIn],
+                previews: latestPosts
+            });
         });
-    });
+    }
 });
 
 router.post('/', function(req, res) {
@@ -86,11 +72,9 @@ router.post('/', function(req, res) {
     }
     var qString = qs.stringify({filter: tags});
     var url = "/?" + qString;
-  //  res.redirect(url);
     res.statusCode = 302;
     res.setHeader("Location", url);
     res.end();
-//    res.send();
 });
 
 router.get('/article', function(req, res, next) {
@@ -113,7 +97,6 @@ router.get('/article', function(req, res, next) {
                 ID: article._id,
                 previews: otherPosts
             };
-            //console.log(accountController.session.userProfileModel);
             if(signedIn && article.author==accountController.session.userProfileModel.username) {
                 articleViewData.deleteLink = "<a href=\"\/delete?ID=" +article._id + "\">Delete</a>"
             }
@@ -146,7 +129,6 @@ router.get('/delete', function(req,res, next) {
             res.send(err);
         }
         post.remove(function (err) {
-            console.log("deleted");
             if(err) {
                 console.log(err);
                 res.send(err);
@@ -205,8 +187,6 @@ router.post('/post', function(req, res, next) {
             var articleURL = "/article?ID=" +  apiResponse.extras.createdPost._id;
             res.redirect(articleURL);
         } else {
-            console.log(apiResponse.extras.msg);
-            console.log(errorMessages[apiResponse.extras.msg]);
             res.render('pages/post',  {
                 viewDataSignStatus: viewDataSignedIn[1],
                 error: errorMessages[apiResponse.extras.msg]
